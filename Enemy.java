f26c09ba641b15ee39e7741197dc600efa6aa1f5
Joonaset <36448219+Joonaset@users.noreply.github.com>
2018-02-15 10:14:16 +0200

    Updated Enemy class with switch

--- Enemy.java
+++ Enemy.java
@@ -4,6 +4,7 @@ public class Enemy {
 	private int wounds;
 	private int tb;
 	private int armor;
+	private String name;
 
 		
 	public Enemy() {
@@ -12,9 +13,33 @@ public class Enemy {
 		tb = 6;
 		armor = 2;
 	}
+	// constructor with class name given as string
+	// correct attributes are given based on class name
+	public Enemy(String classname) {
+		name = classname;
+		switch(name) {
+			case "orkboy":
+				wounds = 12;
+				tb = 6;
+				armor = 2;
+				break;
+			case "stormboy":
+				wounds = 14;
+				tb = 6;
+				armor = 2;
+				break;
+			case "orknob":
+				wounds = 25;
+				tb = 9;
+				armor = 2;
+				break;
+		
+		}
+	}
+		
 	public void subWounds(int damage) {
 		//After taking damage, calculate current wounds
-		wounds =- wounds - (damage - armor )- tb;
+		wounds =-  damage - tb;
 		//calculations are incorrect, output is very wrong
 	}
 	public int getWounds() {
