7fe189a2a9261890cc6aca38239b834bfd1970f2
Joonaset <36448219+Joonaset@users.noreply.github.com>
2018-03-01 11:30:56 +0200

    EnemyTest.java and Enemy.java updated

--- src/Koulu2/Enemy.java
+++ src/Koulu2/Enemy.java
@@ -1,4 +1,4 @@
-package Koulu2;
+package koulu2;
 //Create class Enemy
 public class Enemy {
 	private int wounds;
@@ -19,28 +19,61 @@ public class Enemy {
 		name = classname;
 		switch(name) {
 			case "orkboy":
+				name = "Orkboy";
 				wounds = 12;
 				tb = 6;
 				armor = 2;
 				break;
 			case "stormboy":
+				name = "Stormboy";
 				wounds = 14;
 				tb = 6;
 				armor = 2;
 				break;
 			case "orknob":
+				name = "Orknob";
 				wounds = 25;
 				tb = 9;
 				armor = 2;
 				break;
-		
+			case "guardsman":
+				name = "Guardsman";
+				wounds = 10;
+				tb = 3;
+				armor = 4;
+				break;
+			case "servitor":
+				name = "Servitor";
+				wounds = 10;
+				tb = 4;
+				armor = 4;
+				break;
+			case "ogryn":
+				name = "Ogryn";
+				wounds = 26;
+				tb = 7;
+				armor = 3;
+				break;
 		}
 	}
 		
 	public void subWounds(int damage) {
 		//After taking damage, calculate current wounds
-		wounds =-  damage - tb;
-		//calculations are incorrect, output is very wrong
+		damage = damage - tb;
+		if (damage - tb < 0) {
+			damage = 0;
+		}
+		wounds -=  damage ;
+		//calculations work now without taking armor into account
+	}
+	public String getName() {
+		return name;
+	}
+	public int getTb() {
+		return tb;
+	}
+	public int getArmor() {
+		return armor;
 	}
 	public int getWounds() {
 		return wounds;
--- src/Koulu2/EnemyTest.java
+++ src/Koulu2/EnemyTest.java
@@ -1,24 +1,108 @@
-package Koulu2;
-import java.util.Scanner;
+package koulu2;
+import java.util.*;
 
 public class EnemyTest {
 
 	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
+		int armor;
 		int damage;
 		int ap;
+		int enemynum;
 
-		Enemy ork = new Enemy();
+		//this is list of selectable enemy types
+		ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+		enemies.add(new Enemy("orkboy"));
+		enemies.add(new Enemy("stormboy"));
+		enemies.add(new Enemy("orknob"));
+		enemies.add(new Enemy("guardsman"));
+		enemies.add(new Enemy("servitor"));
+		enemies.add(new Enemy("ogryn")); 
 
+		//print all selectable enemy types
+		for(Enemy i : enemies) {
+			System.out.println("[" + (1 + enemies.indexOf(i)) + "] " + i.getName());
+		}
+		/*
+		switch(int){
+			case 0:
+				selected.add(new Enemy("orkboy"));
+				break;
+			case 1:
+				selected.add(new enemy("stormboy"));
+				break;
+			case 2:
+				selected.add(new enemy("orknob"));
+				break;
+			case 3:
+				selected.add(new enemy("guardsman"));
+				break;
+			case 4:
+				selected.add(new enemy("servitor"));
+		          		break;
+			case 5:
+				selected.add(new enemy("ogryn"));
+				break;
+			}
+			*/
+
+	 	
+		// Test ready-made list of enemies.	
+		ArrayList<Enemy> selected = new ArrayList<Enemy>();
+		selected.add(new Enemy("stormboy"));
+		selected.add(new Enemy("servitor"));		
+		
+		
+		while(selected.size() > 0) {
+			for(Enemy i : selected) {
+				System.out.println("[" + (1 + selected.indexOf(i)) + "] " + i.getName() + " " + i.getWounds());
+			}
+
+			System.out.print("Which enemy takes damage?");
+			int choice = input.nextInt() - 1;
+			Enemy chosen = selected.get(choice);
+			System.out.println("How much damage is inflicted to " + chosen.getName());
+			damage = input.nextInt();
+			System.out.println("How much AP");
+			ap = input.nextInt();
+			armor = chosen.getArmor() - ap;
+			if(armor<0) {
+				armor = 0;
+			}
+			damage = damage - armor;
+			chosen.subWounds(damage);
+			if(chosen.getWounds() <= 0) {
+					System.out.println(chosen.getName() + "died (" + chosen.getWounds() + ")" );
+				selected.remove(chosen);
+			}
+			
+		}
+		System.out.println("Battle over");
+
+		
+		
+		/*
+		Enemy stormBoy = new Enemy("stormboy");
+		System.out.println(stormBoy.getName() + " " + stormBoy.getWounds());
 		System.out.println("Enter damage");
 		damage = input.nextInt();
 		System.out.println("Enter AP");
 		ap = input.nextInt();
 
+		// calculate damage
+		armor = stormBoy.getArmor() - ap;
+		if(armor < 0) {
+			armor = 0;
+		}
+		damage = damage - armor;
 		// call method subWounds
-		ork.subWounds(damage);
-		System.out.print(ork.getWounds());
+		stormBoy.subWounds(damage);
+		System.out.print(stormBoy.getName() + " " + stormBoy.getWounds());
+		*/
 	}
 }
+		
+	
+
 
 
