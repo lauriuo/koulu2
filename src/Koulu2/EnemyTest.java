d627c162226c73b23b7a1399073b4a10d786cfc5
Joonas Taivalmaa <36448219+Joonaset@users.noreply.github.com>
2018-03-05 14:06:30 +0200

    Enemytest should now work as complete CLI-program.

--- src/Koulu2/EnemyTest.java
+++ src/Koulu2/EnemyTest.java
@@ -8,8 +8,9 @@ public class EnemyTest {
 		int armor;
 		int damage;
 		int ap;
-	
-               
+                int choice;
+                ArrayList<Enemy> selected = new ArrayList<Enemy>();
+           
 		//this is list of selectable enemy types
 		ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 		enemies.add(new Enemy("Orkboy"));
@@ -20,26 +21,55 @@ public class EnemyTest {
 		enemies.add(new Enemy("Ogryn")); 
                 
                 
-
-		//print all selectable enemy types
+                System.out.println("Selectable enemy types");
+		//^print all selectable enemy types
 		for(Enemy i : enemies) {
 			System.out.println("[" + (1 + enemies.indexOf(i)) + "] " + i.getName());
 		}
 		
-                
+              
+		do {
+                    System.out.println("Select enemy type, 0 to continue");
+                    choice = input.nextInt();
+                    if(choice == 1) {
+                    selected.add(new Enemy("Orkboy"));
+                    }
+                       else if(choice == 2){
+                                selected.add(new Enemy("Stormboy"));
+                                }
+                      else if(choice ==3){
+                                selected.add(new Enemy("Orknob"));
+                                }
+                     else if(choice == 4){
+                             selected.add(new Enemy("Guardsman"));
+                              }
+                        else if(choice == 5){
+                              selected.add(new Enemy("Servitor"));
+                                }
+                        else if(choice == 6){
+                                selected.add(new Enemy("Ogryn"));
+                            }
+                               //else{ System.out.println("Wrong input, exiting");
+                               //System.exit(0);
+                               //}
+                    for(Enemy i : selected){
+                        System.out.println("[" + (1 + selected.indexOf(i)) + "] " + i.getName() + " " + i.getWounds());
+                    }
+                    } while(choice != 0);
 		// Test ready-made list of enemies.	
-		ArrayList<Enemy> selected = new ArrayList<Enemy>();
-		selected.add(new Enemy("Stormboy"));
-		selected.add(new Enemy("Servitor"));		
+		//
+		//selected.add(new Enemy("Stormboy"));
+		//selected.add(new Enemy("Servitor"));		
 		
+                
 		
 		while(selected.size() > 0) {
-			for(Enemy i : selected) {
-				System.out.println("[" + (1 + selected.indexOf(i)) + "] " + i.getName() + " " + i.getWounds());
-			}
-
-			System.out.print("Which enemy takes damage?");
-			int choice = input.nextInt() - 1;
+                    System.out.println("You have selected following enemy units");
+                    for(Enemy i : selected) {
+                        System.out.println("[" + (1 + selected.indexOf(i)) + "] " + i.getName() + " " + i.getWounds());
+                    }    
+                    System.out.print("Which enemy takes damage?");
+			choice = input.nextInt() - 1;
 			Enemy chosen = selected.get(choice);
 			System.out.println("How much damage is inflicted to " + chosen.getName());
 			damage = input.nextInt();
@@ -52,15 +82,14 @@ public class EnemyTest {
 				armor = 0;
                                
 			}
-                        System.out.println("armor = " + armor);
+                        //System.out.println("armor = " + armor);
 			damage = damage - armor;
                         int tb = chosen.getTb();
-                        System.out.println("TBonus is " + tb);
+                        //System.out.println("TBonus is " + tb);
                         damage = damage - chosen.getTb();
                         //next line is debugging
-                        System.out.printf("Damage is %d \n", damage);
+                        //System.out.printf("Damage is %d \n", damage);
 			chosen.subWounds(damage);
-                        //debugging...
                         
                         
 			if(chosen.getWounds() <= 0) {
@@ -72,8 +101,7 @@ public class EnemyTest {
 		System.out.println("Battle over");
 
 	}
-}	
-	
+}
 
 
 
