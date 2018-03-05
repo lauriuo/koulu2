b823bbcaca2f716d671f9664c4fe258abd7809fb
Joonas Taivalmaa <36448219+Joonaset@users.noreply.github.com>
2018-03-05 14:30:24 +0200

    Added documentation, small fixes for user friendliness

--- src/Koulu2/EnemyTest.java
+++ src/Koulu2/EnemyTest.java
@@ -5,13 +5,16 @@ public class EnemyTest {
 
 	static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
-		int armor;
-		int damage;
-		int ap;
-                int choice;
-                ArrayList<Enemy> selected = new ArrayList<Enemy>();
+                
+		int armor; // this value will be enemy armor points with attacks ap-value subtracted
+		int damage; // player-inflicted damage value
+		int ap; // armor-piercing value
+                
+                int choice; // in CLI-based version, this is used to store users input
+                
+                ArrayList<Enemy> selected = new ArrayList<Enemy>(); // select enemies to be put into this list for a battle.
            
-		//this is list of selectable enemy types
+		//this is list of selectable enemy types. Feel free to add your own. Remember to add them to Enemy.java class too.
 		ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 		enemies.add(new Enemy("Orkboy"));
 		enemies.add(new Enemy("Stormboy"));
@@ -21,14 +24,14 @@ public class EnemyTest {
 		enemies.add(new Enemy("Ogryn")); 
                 
                 
-                System.out.println("Selectable enemy types");
-		//^print all selectable enemy types
+                System.out.println("Select enemy");
+		//print all selectable enemy types
 		for(Enemy i : enemies) {
 			System.out.println("[" + (1 + enemies.indexOf(i)) + "] " + i.getName());
 		}
 		
               
-		do {
+		do { // add enemies until user gives 0.
                     System.out.println("Select enemy type, 0 to continue");
                     choice = input.nextInt();
                     if(choice == 1) {
@@ -49,26 +52,24 @@ public class EnemyTest {
                         else if(choice == 6){
                                 selected.add(new Enemy("Ogryn"));
                             }
-                               //else{ System.out.println("Wrong input, exiting");
-                               //System.exit(0);
-                               //}
+                               else{ System.out.println("Wrong input, try again");}
+                               
+                               
                     for(Enemy i : selected){
                         System.out.println("[" + (1 + selected.indexOf(i)) + "] " + i.getName() + " " + i.getWounds());
                     }
-                    } while(choice != 0);
-		// Test ready-made list of enemies.	
-		//
-		//selected.add(new Enemy("Stormboy"));
-		//selected.add(new Enemy("Servitor"));		
+                    } while(choice != 0);	
 		
+                System.out.println(""); // Print empty line for visual purposes
+		// now program advances to battle. First we print selected units
                 
-		
 		while(selected.size() > 0) {
                     System.out.println("You have selected following enemy units");
                     for(Enemy i : selected) {
                         System.out.println("[" + (1 + selected.indexOf(i)) + "] " + i.getName() + " " + i.getWounds());
                     }    
-                    System.out.print("Which enemy takes damage?");
+                    
+                    System.out.print("Which enemy takes damage?");// ask user to input damage-receiving enemy
 			choice = input.nextInt() - 1;
 			Enemy chosen = selected.get(choice);
 			System.out.println("How much damage is inflicted to " + chosen.getName());
